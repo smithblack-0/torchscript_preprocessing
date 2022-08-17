b13faa20c4af932ece5d2c68a55c45a7b2e5a981
Chris O'Quinn <chrisoquinn.2@gmail.com>
2022-08-17 10:42:10 -0700

    Did more work.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,6 +4,7 @@ The preprocessing algorithm will attempt to resolve
 """
 
 import ast
+import copy
 from dataclasses import dataclass
 
 import astroid
@@ -24,47 +25,95 @@ class FieldTypes(Enum):
     Float = float
     Complex = complex
     EllipsisLit = type(Ellipsis)
+    Node = astroid.NodeNG
     TrueBool = True
     FalseBool = False
     NoneLit = None
-    Literal = "Literal"
 
 @dataclass
 class info_packet():
     """
+    Two responsiblities.
+
+    Store any relevant information about
+
     A packet of information about an astroid
     child and it's originating node plus
     field
     """
     fieldname: str
     parent: astroid.NodeNG
-    child: Any
+    node: Any
     is_list: bool
+    def derivative(self, value: Any):
+        """Creates a copy of the packet, with node changed to value"""
+        return info_packet(self.fieldname,
+                           self.parent,
+                           value,
+                           self.is_list)
+class NodeExplorer():
+    """
+    This class has a single important responsibility
+
+    Explore the nodes which are out there. Return
+    the needed information as an info packet
+    """
+    def iterate_children(self) -> Generator[info_packet, None, None]:
+        """
+        Iterates over the children of an astroid node.
+        Yields information for each child as a childinfo packet.
+        """
+        node = self.node
+        for child in node.get_children():
+            field = node.locate_child(child)
+            attr = getattr(node, field)
+            if isinstance(attr, FieldTypes.List):
+                is_list = True
+            else:
+                is_list = False
+            yield info_packet(field, node, child, is_list)
+    def define_self(self)->info_packet:
+        """Defines oneself as an info packet"""
+        if self.parent is not None
+    def __init__(self, node: astroid.NodeNG):
+        self.node = node
+
 
 
 class NodeEmitter():
     """
-    Used to build output nodes.
-    """
+    This class has two responsibilities
 
+    Store finished nodes, and build
+    the results when so called.
+    """
+    def emit_astroid(self)->astroid.NodeNG:
+        """Emit the associated astroid node"""
+        return self.construction
     def emplace(self, packet: info_packet):
         """
         Place the value onto the node of fieldname
         Sets fieldname if not list
         Appends if fieldname is list.
         """
+        assert self.spec.parent is packet.parent
         fieldname = packet.fieldname
-        value = packet.child
-
-        if not hasattr(self, fieldname) or not isinstance(getattr(self, fieldname), FieldTypes.List):
-            #This is a literal
-            setattr(self, fieldname, value)
+        value = packet.node
+        if isinstance(value, astroid.NodeNG):
+            #Change the parent if it is a node.
+            value = copy.deepcopy(value)
+            value.parent = self.construction
+        if packet.is_list:
+            ls = getattr(self.construction, fieldname)
+            ls.append(value)
         else:
-            #This is a list
-            getattr(self)
+            setattr(self.construction, fieldname, value)
+    def __init__(self, spec: info_packet):
+        assert isinstance(spec.node, astroid.NodeNG)
+        self.spec = spec
+        self.construction = spec.node.__class__()
 
-        if isinstance(getattr(self, fieldname), FieldTypes.List):
-            setattr(self, fieldname, value)
+
 
 
 
@@ -76,7 +125,7 @@ def make_node_in_context(obj: object)->astroid.NodeNG:
     context
     """
 
-def astroid_transform(node: astroid.NodeNG)->Generator[astroid.NodeNG, None, None]:
+def astroid_transform(packet: info_packet)->Generator[astroid.NodeNG, None, None]:
     """
     Transforms something which is an astroid node.
     Yields a generator of astroid nodes as a result
@@ -106,19 +155,11 @@ def iterate_children(node: astroid.NodeNG)->Generator[childinfo_packet, None, No
 
 
 
-def emplace(node: astroid.NodeNG, fieldname: str, value: childinfo_packet):
-    """
-    Place the value onto the fieldname
-    Sets fieldname if not list
-    Appends if fieldname is list.
-    """
-    if isinstance(getattr(node, fieldname), FieldTypes.List):
-        getattr(node, fieldname).append(value)
-
-
 
 def preprocess(obj: object):
     start_node = make_node_in_context(obj)
+    explorer = NodeExplorer(start_node)
+    emitter = NodeEmitter(start_node)
     construction = start_node.__class__()
     construction.parent = start_node.parent
 
@@ -134,7 +175,7 @@ def preprocess(node: astroid.NodeNG):
     while True:
         try:
             packet: childinfo_packet = next(generator)
-            subnode = packet.child
+            subnode = packet.node
             if isinstance(subnode, astroid.NodeNG):
                 stack.append((node, construction, generator))
                 construction = subnode.__class__()
